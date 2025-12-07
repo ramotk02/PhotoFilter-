d3d67f5a0bd65cfe6019732b18f9bbe4a207c72f
Omar taky <omartaky.ot@gmail.com>
2025-12-07 14:53:24 +0100

    Add Apply RGB Filter fct

--- PhotoEditor/MainWindow.xaml.cs
+++ PhotoEditor/MainWindow.xaml.cs
@@ -27,6 +27,8 @@ namespace PhotoEditor
         BitmapImage CurrentImage;
         WriteableBitmap editableImage;
 
+
+        /// Open Explorer to get a pic
         private void OpenImage_Click(object sender, RoutedEventArgs e)
         {
             OpenFileDialog dlg = new OpenFileDialog();
@@ -37,10 +39,41 @@ namespace PhotoEditor
                 CurrentImage = new BitmapImage(new Uri(dlg.FileName));
                 DisplayedImage.Source = CurrentImage;
 
-                MessageBox.Show("Shön Bild ");
+                MessageBox.Show("Good choice !!!!");
 
             }
         }
-      
+
+        // Apply RGB Filter
+        private void ApplyRGB(byte rFactor, byte gFactor, byte bFactor)
+        {
+            if (CurrentImage == null) return;
+
+            editableImage = new WriteableBitmap(CurrentImage); 
+
+            int width = editableImage.PixelWidth;
+            int height = editableImage.PixelHeight;
+            int stride = width * 4;
+
+            byte[] pixels = new byte[height * stride];
+            editableImage.CopyPixels(pixels, stride, 0);
+
+            for (int i=0; i<pixels.Length; i += 4)
+            {
+                byte b = pixels[i];
+                byte g = pixels[i + 1];
+                byte r = pixels[i + 2];
+
+                pixels[i] = (byte)Math.Min(255, b * (bFactor / 100.0));
+                pixels[i + 1] = (byte)Math.Min(255, g * (gFactor / 100.0));
+                pixels[i + 2] = (byte)Math.Min(255, r * (rFactor / 100.0));
+            }
+            editableImage.WritePixels(new Int32Rect(0, 0, width, height), pixels, stride, 0);
+            DisplayedImage.Source = editableImage;
+
+
+
+        }
+
     }
 }
